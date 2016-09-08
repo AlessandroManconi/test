f17aa7756470d52833b9ee072001db0130612912
Alessandro Manconi <amanconi@vivido.it>
2016-09-08 15:32:04 +0200

    Second Commit

---
+++ TeseoUI/TeseoUI.Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TeseoUI.Droid
+{
+	[Activity (Label = "Progetto Teseo", MainLauncher = true, Icon = "@drawable/Logo")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
---
+++ TeseoUI/TeseoUI/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeseoUI
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
